906a2c2029898ab4db9c63e06e0986c70af2b65d
Finlay Durkin <2561004d@student.gla.ac.uk>
2022-02-12 19:29:58 +0000

    rough scoring system in place

--- apps/employees/admin.py
+++ apps/employees/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from apps.employees.models import WordOfTheDayWord
+from apps.employees.models import WordOfTheDayWord, Score, Employees, Team
 
 admin.site.register(WordOfTheDayWord)
+admin.site.register(Score)
+admin.site.register(Team)
+admin.site.register(Employees)
--- apps/employees/views.py
+++ apps/employees/views.py
@@ -1,12 +1,11 @@
-import enum
-import re
 from unicodedata import category
 from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from apps.employees.models import WordOfTheDayWord
+from apps.employees.models import WordOfTheDayWord, Employees, Score, Team
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from utils.scoring import updateTeamScores, getUserScores
 import enchant
 
 
@@ -15,25 +14,27 @@ def index(request):
 
 @login_required
 def wordoftheday(request):
+   
 
-    #random scores - update when database allows it
-    scores = [["Development", 100],
-            ["Human Resources", -5],
-            ["Management", 45],
-            ["Marketing", 12],
-            ["Analytics", 73]]
+    team_scores = updateTeamScores()
 
-    scores.sort(key=lambda x: x[1], reverse=True)
+    current_user = Employees.objects.get(user=request.user)
 
-    words = WordOfTheDayWord.objects.order_by("-height")
-    old_word = words[0].word
-
+    try:
+        words = WordOfTheDayWord.objects.order_by("-height")
+        old_word = words[0].word
+    except IndexError:
+        WordOfTheDayWord.objects.create(word="hello")
+        words = WordOfTheDayWord.objects.order_by("-height")
+        old_word = words[0].word
+    
     d = enchant.Dict("en_UK")
     include_words = []
     exclude_words = []
 
     context_dict = {"words": words}
-    context_dict['scores'] = scores
+    context_dict['teams'] = team_scores
+    context_dict['users'] = getUserScores()
 
     if request.method == "POST":
         
@@ -50,9 +51,22 @@ def wordoftheday(request):
         elif new_word == old_word:
             return HttpResponseRedirect(request.path_info)
         else:
+            # Everything went right - Manage score stuff here-----------------------------------
+      
+            if current_user.score == None:     # Create the user a score record
+                score_record = Score.objects.create()
+                current_user.score = score_record
+                current_user.save()
+            else:
+                score_record = current_user.score
 
-            score = wordScore(old_word, new_word)
-            print(score)
+            score = wordScore(old_word, new_word)   # Calculate the score
+            score_record.wotd += score              # Add the score to the database
+            score_record.save()
+
+            updateTeamScores()
+
+            print("The user's score is:", Employees.objects.get(user=request.user).score.wotd)
 
             WordOfTheDayWord.objects.create(word = new_word, height = words[0].height+1)
             return HttpResponseRedirect(request.path_info)
@@ -64,14 +78,12 @@ def wordoftheday(request):
 def wordScore(old, new):
     difference_count = 0
 
-    if len(old) < len(new):
-        for i, letter in enumerate(new):
-            if letter != old[i]:
-                difference_count += 1
-    else:
-        for i, letter in enumerate(old):
-            if letter != new[i]:
-                difference_count += 1
+    smaller = min([old, new], key=len)
+    bigger = max([old, new], key=len)
+
+    for i in range(len(smaller)):
+        if smaller[i] != bigger[i]: difference_count += 1
     
-    score = 6-difference_count
+    
+    score = 6-difference_count-(len(bigger)-len(smaller))
     return score
