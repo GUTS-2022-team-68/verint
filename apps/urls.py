f72778aecd3e0f6f91402a5ac9374f6ca88ca925
Hwisun Bae <hwisunbae.hb@gmail.com>
2022-02-13 10:36:09 +0000

    register and messages

--- apps/urls.py
+++ apps/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('leaderboard/', views.leaderboard, name='leaderboard'),
+    path('register/', views.register_view, name='register'),
     path('employees/', include('apps.employees.urls')),
+
 
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
-from django.db.models import ObjectDoesNotExist
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from .forms import NewUserForm
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("/")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request=request, template_name="register.html", context={"register_form": form})
 
 
 def login_view(request):
@@ -16,10 +28,10 @@ def login_view(request):
         login(request, user)
         # Redirect to a success page.
         messages.success(request, "Registration successful.")
-        return render(request, 'index.html')
+        return redirect("/")
     else:
         # Return an 'invalid login' error message.
-        messages.error(request, "Unsuccessful registration. Invalid information.")
+        messages.error(request, "Invalid Login")
 
 
 def logout_view(request):
